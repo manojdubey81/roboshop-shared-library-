3480280fa06fbec28c2016e1012b4a5a642d8aa7
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-05-24 22:12:43 -0700

    shared library init file setup

---
+++ vars/python.groovy
@@ -0,0 +1,24 @@
+def lintChecks() {
+    sh '''
+    # We commented this because devs gonna check the failures.
+    #~/node_modules/jslint/bin/jslint.js server.js
+    #pylint *.py
+    echo Lint Check for ${COMPONENT}
+  '''
+}
+
+def call() {
+    pipeline {
+        agent any
+        stages {
+            // For Each Commit
+            stage('Lint Checks') {
+                steps {
+                    script {
+                        lintChecks()
+                    }
+                }
+            }
+        } // End of Stages
+    }
+}
